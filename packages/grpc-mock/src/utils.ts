bb78b364bb3cbe118107879ae1438ce5495df9ad
Evgeniy Kirpichenko <evkirpichenko@gmail.com>
2022-07-05 13:51:50 +0200

    feat: TECH add default error handler

--- packages/grpc-mock/src/utils.ts
+++ packages/grpc-mock/src/utils.ts
@@ -1,3 +1,7 @@
 export function uncapitalize(text: string): string {
   return (text[0]?.toLowerCase() ?? '') + text.slice(1)
 }
+
+export function isErrorWithCode(error: Error): error is Error & { code: string } {
+  return 'code' in error
+}
